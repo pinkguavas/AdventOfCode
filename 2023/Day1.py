bf7735e3b0b46e7e2bb09294ed9f08d6b191dfa5
Jojo Day <116142066+idoaiuo@users.noreply.github.com>
2023-12-01 20:45:28 -0500

    Solutions to Day 1 puzzles

---
+++ 2023/Day1.py
@@ -0,0 +1,37 @@
+import re
+
+# Part 1
+file = open("path", "r+")
+part1 = 0
+for line in file:
+    numbers = []
+    for i in line:
+        if i.isdigit() == True:
+            numbers.append(i)
+    sum = numbers[0] + numbers[len(numbers)-1]
+    part1 += int(sum)
+file.close()
+
+#Part 2
+file = open("path", "r+")
+part2 = 0
+for line in file:
+    numbers = []
+    line = re.sub("one", "o1e", line)
+    line = re.sub("two", "t2o", line)
+    line = re.sub("three", "t3e", line)
+    line = re.sub("four", "f4r", line)
+    line = re.sub("five", "f5e", line)
+    line = re.sub("six", "s6x", line)
+    line = re.sub("seven", "s7n", line)
+    line = re.sub("eight", "e8t", line)
+    line = re.sub("nine", "n9e", line)
+    for i in line:
+        if i.isdigit() == True:
+            numbers.append(i)
+    sum = numbers[0] + numbers[len(numbers)-1]
+    part2 += int(sum)
+file.close()
+
+print(part1)
+print(part2)
